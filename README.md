07ef48c7f54288697cc01aba0e4cc825d9949653
Антон Илларионов <anton.illarionov1991@gmail.com>
2022-07-31 19:54:33 +0300

    F1

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/02mkefsaq4yrit6i?svg=true)](https://ci.appveyor.com/project/trukhachev495/api-testing)
---
+++ src/test/java/ru/netology/rest/SchemaValidation.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class SchemaValidation {
+    @Test
+    public void SchemaValidation(){
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
+    }
+}
